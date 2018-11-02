6a348260d1b66b4be682d1733dff4ee6f860c4d8
GeorgyNikolaev <georgy.nikolaev@saritasa.com>
2018-11-02 18:22:19 +0500

    Splitting home page into components

--- pages/index.vue
+++ pages/index.vue
@@ -4,27 +4,27 @@
       <h1>Get the latest tech news!</h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link :to="`/posts/${1}`">
-        <article class="post-preview">
-          <div class="post-thumbnail" style="background-image: url(https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg);"></div>
-          <div class="post-content">
-            <h1>Post Title</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link :to="`/posts/${2}`">
-        <article class="post-preview">
-          <div class="post-thumbnail" style="background-image: url(https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg);"></div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <p>Preview Text 2</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview
+        v-for="n in 4"
+        :key="n"
+        :id="`${n}`"
+        thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        title="Hello there!"
+        previewText="This is my first post!"
+      />
     </section>
   </div>
 </template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview';
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
 
 <style scoped>
   .intro {
@@ -64,41 +64,5 @@
     flex-wrap: wrap;
     align-items: center;
     justify-content: center;
-  }
-
-  .post-preview {
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 2px #ccc;
-    background-color: white;
-    width: 90%;
-  }
-
-  a {
-    text-decoration: none;
-    color: black;
-  }
-
-  @media (min-width: 850px) {
-    .post-preview {
-      width: 400px;
-      margin: 10px;
-    }
-  }
-
-  .post-thumbnail {
-    width: 100%;
-    height: 200px;
-    background-position: center;
-    background-size: cover;
-  }
-
-  .post-content {
-    padding: 10px;
-    text-align: center;
-  }
-
-  a:hover .post-content,
-  a:active .post-content {
-    background-color: #ccc;
   }
 </style>
