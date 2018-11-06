278cc8c9f1291a2b7288b5b9d58ad734d57ff1ed
GeorgyNikolaev <georgy.nikolaev@saritasa.com>
2018-11-06 17:18:45 +0500

    Using nuxtServerInit()

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,28 +11,10 @@ export default {
   components: {
     PostList
   },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: '1',
-            title: 'First Post',
-            previewText: 'This is our first post!',
-            thumbnail: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
-          },
-          {
-            id: '2',
-            title: 'Second Post',
-            previewText: 'This is our second post!',
-            thumbnail: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
-          }
-        ]
-      })
-    }, 1500);
-  },
-  created() {
-    this.$store.dispatch('setPosts', this.loadedPosts);
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts;
+    }
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -14,6 +14,27 @@ const createStore = () => {
       }
     },
     actions: {
+      nuxtServerInit(vuexContext, context) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContext.commit('setPosts', [
+              {
+                id: '1',
+                title: 'First Post',
+                previewText: 'This is our first post!',
+                thumbnail: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
+              },
+              {
+                id: '2',
+                title: 'Second Post',
+                previewText: 'This is our second post!',
+                thumbnail: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
+              }
+            ]);
+            resolve();
+          }, 1500)
+        })
+      },
       setPosts: ({ commit }, posts) => {
         commit('setPosts', posts)
       }
