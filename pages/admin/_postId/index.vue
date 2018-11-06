5302a1165e63650cb131af8a7b6dba71d5e9884a
GeorgyNikolaev <georgy.nikolaev@saritasa.com>
2018-11-06 19:11:37 +0500

    Fetching and adding data to Firebase with synchronizing Vuex

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="admin-post-page">
     <section class="update-form">
-      <AdminPostForm :post="loadedPost"/>
+      <AdminPostForm :post="loadedPost" @submit="onSubmitted"/>
     </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import AdminPostForm from '@/components/Admin/AdminPostForm';
 
 export default {
@@ -14,14 +15,20 @@ export default {
   components: {
     AdminPostForm
   },
-  data() {
-    return {
-      loadedPost: {
-        author: 'gnklv',
-        title: 'My awesome Post',
-        content: 'Super amazing, thanks for that',
-        thumbnailLink: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
-      }
+  asyncData(context) {
+    return axios.get(`https://nuxt-blog-gnklv.firebaseio.com/posts/${context.params.postId}.json`)
+      .then(res => {
+        return {
+          loadedPost: { ...res.data, id: context.params.postId }
+        }
+      })
+      .catch(e => context.error(e))
+  },
+  methods: {
+    onSubmitted(editedPost) {
+      this.$store.dispatch('editPost', editedPost).then(() => {
+        this.$router.push('/admin');
+      });
     }
   }
 }
--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from 'vuex';
+import axios from 'axios';
 
 const createStore = () => {
   return new Vuex.Store({
@@ -11,32 +12,44 @@ const createStore = () => {
     mutations: {
       setPosts: (state, posts) => {
         state.loadedPosts = posts
+      },
+      addPost: (state, post) => {
+        state.loadedPosts.push(post);
+      },
+      editPost: (state, editedPost) => {
+        const postIndex = state.loadedPosts.findIndex(post => post.id === editedPost.id);
+        state.loadedPosts[postIndex] = editedPost;
       }
     },
     actions: {
-      nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            vuexContext.commit('setPosts', [
-              {
-                id: '1',
-                title: 'First Post',
-                previewText: 'This is our first post!',
-                thumbnail: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
-              },
-              {
-                id: '2',
-                title: 'Second Post',
-                previewText: 'This is our second post!',
-                thumbnail: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
-              }
-            ]);
-            resolve();
-          }, 1500)
-        })
+      nuxtServerInit({ commit }, context) {
+        return axios.get('https://nuxt-blog-gnklv.firebaseio.com/posts.json')
+          .then(res => {
+            const postsArray = [];
+            for (const key in res.data) {
+              postsArray.push({ ...res.data[key], id: key })
+            }
+            commit('setPosts', postsArray);
+          })
+          .catch(e => context.error(e));
       },
       setPosts: ({ commit }, posts) => {
-        commit('setPosts', posts)
+        commit('setPosts', posts);
+      },
+      addPost: ({ commit }, post) => {
+        const createdPost = { ...post, updatedDate: new Date() };
+        return axios.post('https://nuxt-blog-gnklv.firebaseio.com/posts.json', createdPost)
+          .then(res => {
+            commit('addPost', { ...createdPost, id: res.data.name });
+          })
+          .catch(e => console.log(e));
+      },
+      editPost: ({ commit }, editedPost) => {
+        axios.put(`https://nuxt-blog-gnklv.firebaseio.com/posts/${editedPost.id}.json`, editedPost)
+          .then(() => {
+            commit('editPost', editedPost);
+          })
+          .catch(e => console.log(e));
       }
     }
   })
